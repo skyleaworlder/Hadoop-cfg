ce6931e6faee3dbf7c472227b19d30be6d82a1d0
skyleaworlder <870033938@qq.com>
2021-03-22 00:49:44 +0800

    M: add comments

--- node_before_script.sh
+++ node_before_script.sh
@@ -16,7 +16,11 @@
 #       - install java & hadoop
 #       - check integrity
 
-# 0. create a folder to make a backup for some "etc" files
+# 0. create a folder to make a backup for some "etc" files:
+#   ~/hadoop_bak/:
+#       - .bashrc: ~/.bashrc
+#       - hosts: /etc/hosts
+#       - sources.list: /etc/apt/sources.list
 BAK_PATH=~/hadoop_bak
 mkdir $BAK_PATH
 sudo cp /etc/hosts $BAK_PATH
@@ -24,9 +28,11 @@ sudo cp /etc/apt/sources.list $BAK_PATH
 sudo cp ~/.bashrc $BAK_PATH
 
 # 1. Change hostname of ECS
-# e.g. if IP of your UCloud-ECS is 10.23.169.137, your hostname will be 10-23-159-192
-# you can execute "hostname" over SSH to get the hostname of your ECS
-# For master node, "master" is set as hostname here.
+# e.g. if IP of your UCloud-ECS is 10.23.169.137, your hostname will be initialized as 10-23-159-192
+# you can execute "hostname" on your ECS to get the hostname.
+# your cluster has 3 nodes normally in this lab.
+# For master node, "master" can be set as hostname here.
+# For slave node, "slave1" and "slave2" are recommended.
 echo "[$1]: Your hostname will be replaced by $1."
 sudo hostnamectl set-hostname $1
 
@@ -41,7 +47,9 @@ echo "[$1]: Begin to 'copy' your id_rsa.pub(public key used in ssh) to other mac
 read -p "Please input master's LAN-IP: " masterIP
 read -p "Please input the number of slaves: " slaveNum
 echo "$masterIP master" > .tmp.ipcfg
+# distribute ssh public key to master node
 ssh-copy-id -i ~/.ssh/id_rsa.pub ubuntu@$masterIP
+
 i=1
 while [ $i -le $slaveNum ]
 do
@@ -49,7 +57,7 @@ do
     read -p "Please input the number of above slave: " slaveNo
     echo "$slaveIP  slave$slaveNo" >> .tmp.ipcfg
 
-    # distribute ssh public key
+    # distribute it to slave node
     ssh-copy-id -i ~/.ssh/id_rsa.pub ubuntu@$slaveIP
     ((i=i+1))
 done
@@ -57,6 +65,7 @@ sudo bash -c "cat .tmp.ipcfg >> /etc/hosts"
 
 # 4. Change /etc/apt/sources.list
 # focal --> Ubuntu 20.04
+# proposed and src are "commented".
 (
 cat <<EOF
 deb http://mirrors.163.com/ubuntu/ focal main restricted universe multiverse
@@ -76,6 +85,10 @@ sudo apt-get update
 sudo apt-get upgrade
 
 # 5. install jdk
+# Normally, "java -version" print:
+#   openjdk version "1.8.0_282"
+#   OpenJDK Runtime Environment (balabala)
+#   OpenJDK 64-Bit Server VM (balabala)
 sudo apt install openjdk-8-jdk-headless
 if [ $(java -version 2>&1 | grep -c "openjdk version") -ge 1 ];then
     echo "jdk install successfully."
